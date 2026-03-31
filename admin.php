6be39ad737a36acb4853d9ce7f61604732ff4237
Bob <your_email@example.com>
2026-03-31 11:29:20 +0300

    a

--- admin.php
+++ admin.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$title = "Админ-панель - Гостевой дом 'Уют'";
+$title = 'Админ-панель - Гостевой дом "Уют"';
 session_start(); // Только один вызов
 
 // Проверка авторизации администратора
