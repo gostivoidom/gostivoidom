302f3ed7682899fd1ea89afa39d4a821c4363e2c
Bob <your_email@example.com>
2026-03-31 12:09:31 +0300

    ss

--- db/connect.php
+++ db/connect.php
@@ -35,6 +35,7 @@ $conn->query("CREATE TABLE IF NOT EXISTS rooms (
     name VARCHAR(255) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     capacity INT NOT NULL,
+    description TEXT,
     image_url VARCHAR(255)
 )");
 
